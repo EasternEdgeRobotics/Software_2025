afb50cbff6c0c12f93195c3d3d2c22ba1eedf8a6
Zaid-Duraid <123819026+Zaid-Duraid@users.noreply.github.com>
2024-10-19 10:10:16 -0230

    initialized repository with 2024 package

---
+++ WebGUI/src/api/Atoms.tsx
@@ -0,0 +1,88 @@
+import { atom } from "jotai";
+
+
+// In the React Javascript Framework, normal Javascript variables still exist. However, a new type of variable is introduced which is called a state.
+// When a state is changed, components using that state (the same way you'd use a normal variable) are updated.
+
+// States are local to the scope of the function they are defined in. However, using the Atom states from the jotai library allows for the creating of global states
+// Which can be accessed in multiple files
+
+
+// Global State Definitions
+export const CameraURLs = atom<string[]>(["", "", "", ""]);
+export const ROSIP = atom<string>(localStorage.getItem("ROS_IP") || window.location.hostname);
+export const IsROSConnected = atom<boolean>(false);
+
+export const ImuData = atom<string>("");
+
+export const Mappings = atom<{ [controller: number]: { [type: string]: { [index: number]: string } } }>({ 0: {}, 1: {} }); // Current controller mappings
+export const KeyboardMode = atom<boolean>(false);
+
+export const ThrusterMultipliers = atom<number[]>([20, 0, 0, 0, 0, 0]); // Power:0, Surge:1, Sway:2, Heave:3, Pitch:4, Yaw:5
+export const ControllerInput = atom<(number | undefined)[]>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // Current controller input from pilot
+export const PilotActions = atom<string[]>([ // Possible pilot inputs
+  "None",
+  "surge",
+  "sway",
+  "heave",
+  "pitch",
+  "yaw",
+  "open_claw",
+  "close_claw",
+  "heave_up",
+  "heave_down",
+  "pitch_up",
+  "pitch_down",
+  "brighten_led",
+  "dim_led",
+  "turn_stepper_cw",
+  "turn_stepper_ccw",
+  "read_outside_temperature_probe",
+  "enter_auto_mode"
+])
+
+export const KeyboardInputMap = atom<(string | number)[][]>([ // Possible pilot inputs
+  ["w", "surge", 100],
+  ["a", "sway", -100],
+  ["s", "surge", -100],
+  ["d", "sway", 100],
+  ["q", "yaw", -100],
+  ["e", "yaw", 100],
+  ["w", "surge", 100],
+  ["r", "heave", 100],
+  ["f", "heave", -100],
+  ["t", "pitch", 100],
+  ["g", "pitch", -100],
+  ["z", "open_claw", 1],
+  ["x", "close_claw", 1],
+  ["c", "brighten_led", 1],
+  ["v", "dim_led", 1],
+  ["b", "turn_stepper_cw", 1],
+  ["n", "turn_stepper_ccw", 1],
+  ["m", "read_outside_temperature_probe", 1],
+  [",", "enter_auto_mode", 1],
+])
+
+export const CurrentProfile = atom<string>("Not Assigned"); // Current pilot profile
+export const ProfilesList = atom<[{ id: number, name: string, controller1: string, controller2: string }]>([{ id: 0, name: "default", controller1: "null", controller2: "null" }]); // List of known pilot profiles
+
+export const RequestingConfig = atom<{ state: number, profileName: string, controller1: string, controller2: string }>({ state: 2, profileName: "default", controller1: "null", controller2: "null" }); // Used for requesting mappings from OR loading mappings into the Database
+export const RequestingProfilesList = atom<number>(2); // Used for deleting a certain profile in the database or requesting a list of profiles.
+export const RequestingCameraURLs = atom<number>(2); // Used for requesting/saving camera URLs from/to the database.
+// See the ROS.tsx script for how the above three Atom states are used
+
+export const ADCArray = atom<{ adc_48v_bus: number, adc_12v_bus: number, adc_5v_bus: number }>({ adc_48v_bus: 0, adc_12v_bus: 0, adc_5v_bus: 0 }); // ADC DATA
+export const TemperatureArray = atom<{
+  power_board_u8: number, power_board_u9: number, power_board_u10: number,
+  mega_board_ic2: number, power_board_u11: number, mega_board_ic1: number
+}>({
+  power_board_u8: 0, power_board_u9: 0, power_board_u10: 0,
+  mega_board_ic2: 0, power_board_u11: 0, mega_board_ic1: 0
+}); // TEMP DATA
+export const IMUArray = atom<{ temperature: number, acceleration: number[], magnetic: number[], 
+  euler: number[], linear_acceleration: number[]}>({
+    temperature: 0, acceleration: [0,0,0], 
+    magnetic: [0,0,0], euler: [0,0,0], linear_acceleration: [0,0,0]}); // IMU DATA 
+
+export const OutsideTemperatureProbe = atom<number>(0);
+export const AutonomousModeStatus = atom<string>("Autonomous Mode Off"); 
---
+++ WebGUI/src/components/App.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, Tab, Tabs } from "@mui/material";
+import { ArrowUpRightSquare, Bot, Camera, Wrench } from "lucide-react";
+import { useState } from "react";
+import CameraTab from "./CameraTab";
+import SettingsTab from "./SettingsTab";
+import React from "react";
+import { useAtom } from "jotai";
+import { CameraURLs } from "../api/Atoms";
+import SafetyDisclaimer from "./SafetyDisclaimer";
+import { InitROS } from "../api/ROS";
+import { BotTab } from "./BotTab";
+
+// Navbar, calls to render each window, and a popout button
+
+export function RenderTab(props: { tab: number }) {
+    //return respective tab depending on which tab is chosen, or else return null (which should never happen)
+    switch (props.tab) {
+        case 0: return (<CameraTab />);
+        case 1: return (<BotTab />);
+        case 2: return (<SettingsTab />);
+        default: return (null);
+    }
+}
+
+export default function App() {
+    const [tabIndex, setTabIndex] = useState<number>(0);
+    const [, setCameraURLs] = useAtom(CameraURLs);
+
+    React.useEffect(() => {
+        /**fetch('/config').then(response => response.json()).then(data => {
+            setCameraURLs(data.CameraURLs || ["", "", ""]);
+       });**/
+    }, [setCameraURLs]);
+
+    return (
+        <Box>
+            <InitROS /> {/* Initialize ROS */}
+            <SafetyDisclaimer /> {/* Display the safety disclaimer */}
+            <Tabs value={tabIndex} onChange={(_, index) => { setTabIndex(index); }} centered> {/* Navbar on the top of the screen */}
+                <Tab label={<Camera />} />
+                <Tab label={<Bot />} />
+                <Tab label={<Wrench />} />
+            </Tabs>
+            <br />
+            {/* Popout window button, only for camera and bot tasks*/}
+            <Box position="absolute" top="8px" right="8px">
+                {/* To allow new tabs for popout, change index at the array below */}
+                <Button variant="outlined" disabled={![0, 1].includes(tabIndex)} onClick={() => { window.open(`/${tabIndex}`, "", "popout"); }}><ArrowUpRightSquare /></Button>
+            </Box>
+            {/* Pass through the tab to the render function in order to render the component */}
+            <RenderTab tab={tabIndex} />
+        </Box>
+    );
+}
