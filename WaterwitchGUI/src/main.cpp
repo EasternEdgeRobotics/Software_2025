8b179e7c2cae3e4fc2a757e5eecbcec51f790d59
Evan <41494790+evaan@users.noreply.github.com>
2024-12-19 21:15:07 -0330

    Config

--- WaterwitchGUI/src/main.cpp
+++ WaterwitchGUI/src/main.cpp
@@ -4,6 +4,12 @@
 #include "backends/imgui_impl_opengl3.h"
 #include <iostream>
 #include "Config.hpp"
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <filesystem>
+
+using json = nlohmann::json;
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
 Config config;
 
@@ -11,10 +17,9 @@ bool showConfigWindow = false;
 bool showCameraWindow = false;
 bool showAboutWindow = true;
 
-vector<ButtonAction> buttonActions;
-vector<AxisAction> axisActions;
-
 int main() {
+    filesystem::create_directory("configs");
+
     if (!glfwInit()) return -1;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -61,11 +66,28 @@ int main() {
                     showConfigWindow = true;
                 }
                 if (ImGui::BeginMenu("Load Config")) {
-                    for (int i = 0; i < 5; i++) {
-                        if (ImGui::MenuItem(std::to_string(i+1).c_str())) {
-                            //TODO: load config
+                    for (const auto& configFile : recursive_directory_iterator("configs")) {
+                        if (ImGui::MenuItem(configFile.path().filename().string().c_str())) {
+                            ifstream configJson(string("configs/") + configFile.path().filename().string());
+                            json configData = json::parse(configJson);
+                            std::strncpy(config.cam1ip, configData["cameraIPs"][0].get<std::string>().c_str(), sizeof(config.cam1ip) - 1);
+                            std::strncpy(config.cam2ip, configData["cameraIPs"][1].get<std::string>().c_str(), sizeof(config.cam2ip) - 1);
+                            std::strncpy(config.cam3ip, configData["cameraIPs"][2].get<std::string>().c_str(), sizeof(config.cam3ip) - 1);
+                            config.cam1ip[sizeof(config.cam1ip) - 1] = '\0';
+                            config.cam2ip[sizeof(config.cam2ip) - 1] = '\0';
+                            config.cam3ip[sizeof(config.cam3ip) - 1] = '\0';
+                            std::vector<int> buttons = configData["buttons"].get<std::vector<int>>();
+                            config.buttonActions.clear();
+                            for (int i = 0; i < buttons.size(); i++) {
+                                config.buttonActions.push_back(static_cast<ButtonAction>(buttons[i]));
+                            }
+                            std::vector<int> axes = configData["axes"].get<std::vector<int>>();
+                            config.axisActions.clear();
+                            for (int i = 0; i < axes.size(); i++) {
+                                config.axisActions.push_back(static_cast<AxisAction>(axes[i]));
+                            }
                         }
-                    } 
+                    }
                     ImGui::EndMenu();
                 }
                 ImGui::EndMenu();                
@@ -100,29 +122,29 @@ int main() {
                         int buttonCount, axisCount;
                         const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
                         const float* axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axisCount);
-                        if (buttonCount > 0 && buttonActions.empty()) buttonActions = vector<ButtonAction>(buttonCount, ButtonAction::NONE);
-                        if (axisCount > 0 && axisActions.empty()) axisActions = vector<AxisAction>(axisCount, AxisAction::NONE);
+                        if (buttonCount > 0 && config.buttonActions.empty()) config.buttonActions = vector<ButtonAction>(buttonCount, ButtonAction::NONE);
+                        if (axisCount > 0 && config.axisActions.empty()) config.axisActions = vector<AxisAction>(axisCount, AxisAction::NONE);
                         ImGui::Text("Connected Controller: %s", glfwGetJoystickName(GLFW_JOYSTICK_1));
                         ImGui::Text("Axis Deadzone");
                         ImGui::SameLine();
                         ImGui::SliderFloat("##deadzone", &config.deadzone, 0.f, 1.f);
                         ImGui::SeparatorText("Buttons");
                         for (int i = 0; i < buttonCount; i++) {
-                            if (buttons[i]) ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0, 1, 0, 1));
+                            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, !buttons[i], 1, 1));
                             ImGui::Text((std::string("Button ") + std::to_string(i)).c_str());
-                            if (buttons[i]) ImGui::PopStyleColor();
+                            ImGui::PopStyleColor();
                             ImGui::SameLine(); 
-                            if (ImGui::Combo((std::string("##button") + std::to_string(i)).c_str(), reinterpret_cast<int*>(&buttonActions[i]), buttonActionLabels, static_cast<int>(ButtonAction::SIZE))) {
+                            if (ImGui::Combo((std::string("##button") + std::to_string(i)).c_str(), reinterpret_cast<int*>(&config.buttonActions[i]), buttonActionLabels, static_cast<int>(ButtonAction::SIZE))) {
 
                             }
                         }
                         ImGui::SeparatorText("Axes");
                         for (int i = 0; i < axisCount; i++) {
-                            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0, abs(axes[i]), 0, 1)); //TODO: this is kind of ugly
+                            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 1-abs(axes[i]), 1, 1)); //TODO: this is kind of ugly
                             ImGui::Text((std::string("Axis ") + std::to_string(i)).c_str());
                             ImGui::PopStyleColor();
                             ImGui::SameLine();
-                            if (ImGui::Combo((std::string("##axis") + std::to_string(i)).c_str(), reinterpret_cast<int*>(&axisActions[i]), axisActionLabels, static_cast<int>(AxisAction::SIZE))) {
+                            if (ImGui::Combo((std::string("##axis") + std::to_string(i)).c_str(), reinterpret_cast<int*>(&config.axisActions[i]), axisActionLabels, static_cast<int>(AxisAction::SIZE))) {
                                 
                             }
                         }
@@ -134,7 +156,28 @@ int main() {
                 if (ImGui::BeginTabItem("Save")) {
                     ImGui::Text("Config Name");
                     ImGui::SameLine(); 
-                    ImGui::InputText("##configName", config.cam1ip, 64);
+                    ImGui::InputText("##configName", config.name, 64);
+                    if (!glfwJoystickPresent(GLFW_JOYSTICK_1)) {
+                        ImGui::Text("Controller must be connected");
+                    } else if (ImGui::Button("Save")) {
+                        json configJson;
+                        configJson["name"] = config.name;
+                        configJson["cameraIPs"][0] = config.cam1ip;
+                        configJson["cameraIPs"][1] = config.cam2ip;
+                        configJson["cameraIPs"][2] = config.cam3ip;
+                        configJson["controller"] = glfwGetJoystickName(GLFW_JOYSTICK_1);
+                        configJson["deadzone"] = config.deadzone;
+                        for (int i = 0; i < config.buttonActions.size(); i++) {
+                            configJson["buttons"][i] = config.buttonActions[i];
+                        }
+                        for (int i = 0; i < config.axisActions.size(); i++) {
+                            configJson["axes"][i] = config.axisActions[i];
+                        }
+                        std::ofstream ofs;
+                        ofs.open("configs/" + string(config.name) + ".json", std::ofstream::out | std::ofstream::trunc);
+                        ofs << configJson.dump();
+                        ofs.close();
+                    }
                     ImGui::EndTabItem();
                 }
                 ImGui::EndTabBar();
