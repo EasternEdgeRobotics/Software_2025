c3eaed7b0d174123f939411ffcacf058fc219e74
Zaid-Duraid <zaidahmedduraid@gmail.com>
2024-12-24 21:39:33 -0330

    feat: working waterwitch thruster math

--- ROS2/ROS_workspace/src/waterwitch_pkg/src/waterwitch_constants.cpp
+++ ROS2/ROS_workspace/src/waterwitch_pkg/src/waterwitch_constants.cpp
@@ -1,12 +1,12 @@
 #include "waterwitch_constants.h"
 
 const int8_t THRUSTER_CONFIG_MATRIX[6][6] = {
-    {1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, -1},
-    {0, 1, 0, 0, 1, 0},
-    {0, 1, 0, 0, -1, 0},
-    {0, 0, 1, 1, 0, 0},
-    {0, 0, 1, -1, 0, 0}
+    {1, -1, 0, 0, 0, 1}, // for star
+    {1, 1, 0, 0, 0, -1}, // for port
+    {-1, -1, 0, 0, 0, -1}, // aft star
+    {-1, 1, 0, 0, 0, 1}, // aft port
+    {0, 0, 1, 1, 0, 0}, // star top
+    {0, 0, 1, -1, 0, 0} // port top
 };
 
 const std::string THRUSTER_NAMES[6] = {
