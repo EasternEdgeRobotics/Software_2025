85415356085d55a541fe6617051f3f09d1de46f8
LoganJanes <lmjanes@mun.ca>
2025-01-18 11:56:16 -0330

    Proper Animation
    Proper tiles are now filled in for each year in the animation

--- web_frontend/src/components/CarpTab.tsx
+++ web_frontend/src/components/CarpTab.tsx
@@ -37,22 +37,11 @@ const regions: Record<string, number[][]> = {
 const graphData = {
   Year: [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
   Regions: {
-    "Region 1": [false, true, true, true, true, true, true, true, true, true],
-    "Region 2": [false, false, true, true, true, true, true, true, true, true],
-    "Region 3": [false, false, false, true, true, true, true, true, true, true],
-    "Region 4": [false, false, false, false, true, true, true, true, true, true],
-    "Region 5": [
-      false,
-      false,
-      false,
-      false,
-      false,
-      true,
-      true,
-      true,
-      true,
-      false,
-    ],
+    "Region 1": [false, true,  true, true, true, true, true, true, true, true],
+    "Region 2": [false, false, false, false, true, true, true, true, true, true],
+    "Region 3": [false, false, false, false, true, true, true, true, true, true],
+    "Region 4": [false, false, false, false, false, false, false, true, true, true],
+    "Region 5": [false, false, false, false, false, false, false, false, false, false, ],
   },
 };
 
