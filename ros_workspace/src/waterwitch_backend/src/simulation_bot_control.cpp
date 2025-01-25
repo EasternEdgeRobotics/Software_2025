f01440b98e26b3b5ab0c3286f4b421e2aff49666
Nife-Olaiya <cath.olaiya@gmail.com>
2025-01-25 15:32:27 -0330

    Added publisher and subscriber to simulation_bot_control

--- ros_workspace/src/waterwitch_backend/src/simulation_bot_control.cpp
+++ ros_workspace/src/waterwitch_backend/src/simulation_bot_control.cpp
@@ -1,11 +1,14 @@
 #include <memory>
 #include <thread>
 #include <vector>
+#include<bits/stdc++.h> 
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "eer_interfaces/msg/waterwitch_control.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 #include "waterwitch_constants.h"
 
 
@@ -23,6 +26,8 @@ public:
     front_camera_downtilt_servo_publisher = this->create_publisher<std_msgs::msg::Float64>("/front_camera_downtilt_servo", 10);
     back_camera_forward_servo_publisher = this->create_publisher<std_msgs::msg::Float64>("/back_camera_forward_servo", 10);
     back_camera_downtilt_servo_publisher = this->create_publisher<std_msgs::msg::Float64>("/back_camera_downtilt_servo", 10);
+    
+    imu_publisher = this->create_publisher<std_msgs::msg::String>("/diagnostics_data_1", 10);
 
     auto control_values_subscriber_callback =
       [this](eer_interfaces::msg::WaterwitchControl::UniquePtr control_values_msg) -> void {
@@ -59,16 +64,49 @@ public:
 
     control_values_subscriber =
       this->create_subscription<eer_interfaces::msg::WaterwitchControl>(
-        "waterwitch_control_values", 10, control_values_subscriber_callback);
+        "waterwitch_control_values", 10, control_values_subscriber_callback); 
+    
+    auto imu_subscriber_callback =
+      [this](Sensor_msgs::msg::Imu::UniquePtr imu_data_msg) -> void {
+
+        std_msgs::msg::String diagnostics_data_msg;
+
+        // Map the thrust value from the range [-1, 1] to [0, 255]
+        diagnostics_data_msg.data += "Orientation: [" + std::to_string(imu_data_msg->orientation.x) + ","
+                                    std::to_string(imu_data_msg->orientation.y) + ","
+                                    std::to_string(imu_data_msg->orientation.z) + ","
+                                    std::to_string(imu_data_msg->orientation.w) + "]\n";
+
+         diagnostics_data_msg.data += "Angular velocity: [" + std::to_string(imu_data_msg->angular_velocity.x) + ","
+                                    std::to_string(imu_data_msg->angular_velocity.y) + ","
+                                    std::to_string(imu_data_msg->angular_velocity.z) + "]\n";      
+
+        diagnostics_data_msg.data += "Linear acceleration: [" + std::to_string(imu_data_msg->linear_acceleration.x) + ","
+                                    std::to_string(imu_data_msg->linear_acceleration.y) + ","
+                                    std::to_string(imu_data_msg->linear_acceleration.z) + "]";                     
+        
+
+        imu_publisher->publish(diagnostics_data_msg);
+      };
+
+      imu_subscriber = 
+      this->create_subscription<sensor_msgs::msg::Imu>(
+        "imu", 10, imu_subscriber_callback);
   }
+
+  
+
 
 private:
   rclcpp::Subscription<eer_interfaces::msg::WaterwitchControl>::SharedPtr control_values_subscriber;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr imu_publisher;
+  rclcpp::Subscriber<sensor_msgs::msg::Imu>::SharedPtr imu_subscriber;
   std::array<rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr, 6> thruster_publishers;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr front_camera_forward_servo_publisher;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr front_camera_downtilt_servo_publisher;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr back_camera_forward_servo_publisher;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr back_camera_downtilt_servo_publisher;
+    
 };
 
 int main(int argc, char * argv[])
