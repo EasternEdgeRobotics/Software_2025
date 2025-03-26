f6a5acd1cd0b24fefc173887e61e2e7e4682afa6
Zaid Duraid <123819026+Zaid-Duraid@users.noreply.github.com>
2025-03-25 23:15:50 -0230

    fix: thruster config matrix roll

--- ros_workspace/src/waterwitch_backend/src/waterwitch_constants.cpp
+++ ros_workspace/src/waterwitch_backend/src/waterwitch_constants.cpp
@@ -14,8 +14,8 @@ const int8_t THRUSTER_CONFIG_MATRIX[6][6] = {
     {-1, 1, 0, 0, 0, 1}, // for port
     {1, -1, 0, 0, 0, 1}, // aft star
     {1, 1, 0, 0, 0, -1}, // aft port
-    {0, 0, -1, 1, 0, 0}, // star top
-    {0, 0, -1, -1, 0, 0} // port top
+    {0, 0, -1, 0, 1, 0}, // star top
+    {0, 0, -1, 0, -1, 0} // port top
 };
 
 const std::string THRUSTER_NAMES[6] = {
