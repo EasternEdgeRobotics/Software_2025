561b93c8c8f2a3da8e0288966cd7d005a43d18bd
Evan Whelan <41494790+evaan@users.noreply.github.com>
2025-06-05 14:32:20 -0230

    Center cameras

--- science_officer_frontend/src/CameraTab.tsx
+++ science_officer_frontend/src/CameraTab.tsx
@@ -2,16 +2,19 @@ import { Box, Grid } from "@mui/material";
 
 export function AltCamera(props: { ip: string }) {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        aspectRatio: "16 / 9",
-        maxHeight: "45vh",
-        backgroundImage: `url(${props.ip}), url(./nosignal.jpg)`,
-        backgroundSize: "100% 100%",
-        borderRadius: "12px",
-      }}
-    />
+    <Box width="100%" height="100%" display="flex" justifyContent="center" minHeight="45vh">
+      <Box
+        sx={{
+          aspectRatio: "16 / 9",
+          maxHeight: "45vh",
+          backgroundImage: `url(${props.ip}), url(./nosignal.jpg)`,
+          backgroundSize: "cover",
+          borderRadius: "12px",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+    </Box>
   );
 }
 
